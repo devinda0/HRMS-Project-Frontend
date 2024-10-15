85993e1b18c37bef3d8ec80472141dd7f2c33045
Dilhara Disanayaka <dilhara.22@cse.mrt.ac.lk>
2024-10-15 13:40:14 +0530

    Add files via upload

---
+++ src/Components/Dependentinfo/Dependentinfo.jsx
@@ -0,0 +1,87 @@
+import { BiAddToQueue } from "react-icons/bi";
+import { FiEdit } from "react-icons/fi"
+
+import React from 'react';
+
+import { useState,useContext } from "react";
+
+import Popups from "../Popups/Popups";
+
+function Dependent_info({edit}){
+   
+    
+    const [showPopup, setShowPopup] = useState(false);
+    
+    const closePopup = () => {
+        setShowPopup(false);
+    };
+    const dependants=[
+        {
+            name:"Dhruva Doe",
+            relation:"son"
+        },
+        {
+          name:"Samua Doe",
+          relation:"son"
+        },
+        {
+            name:"Samua Doe",
+            relation:"son"
+        }
+    ]
+    
+    
+    return (
+        <>
+        <div class="table-container">
+        <h1 className="h2">Dependant Information</h1>    
+        <table>
+            <thead>
+                <tr className="headers">
+                    <th>Dependent's Name</th>
+                    <th>Relation to Employee</th>
+                {(edit==true)&&(    
+                    <th>
+                        <button  type="button" class="add-btn" onClick={()=>setShowPopup(true)}>
+                            < BiAddToQueue className="icon1"/>
+                        </button>
+                    </th>
+                  )}      
+                </tr>
+              
+            </thead>
+            <tbody  className="raws">
+                {dependants.map(dependant=>(
+                    <tr>
+                    <td>{dependant.name}</td>
+                    <td>{dependant.relation}</td>
+                    {(edit==true)&&( 
+                    <td>
+                        
+                    <button type="button" class="edit-btn" onClick={()=>setShowPopup(true)}>
+                            < FiEdit className="icon"/>
+                        </button>
+                    
+                    </td>
+                    )}
+
+                </tr>
+
+                ))}
+                
+            </tbody>
+        </table>
+        
+    </div>
+   
+    {showPopup && (
+        <div>
+          <Popups closePopup={closePopup} /> {/* Popup with a close function */}
+        </div>
+     )}
+      
+        </>
+      );
+ };
+      
+export default Dependent_info
