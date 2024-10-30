b5acf277b9f34bc0615ae6ecfb53d7dd71f8e17f
Janindu Dissanayaka <janidu.22@cse.mrt.ac.lk>
2024-10-30 19:46:36 +0530

    few changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex items-center justify-between px-6 py-3 bg-white shadow-md">
+    <nav className="flex items-center justify-between px-6 py-3 bg-white shadow-md z-50">
       
       <div className="flex items-center space-x-8 ">
         <img src={logo} alt="" className="w-30 h-10" />
